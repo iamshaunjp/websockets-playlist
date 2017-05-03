6afe82ec4764e90c70e64b9beb8f1850f2aa550f
iamshaunjp <shaunjp@shaun-pelling.com>
2017-05-03 20:48:28 +0100

    added lesson 2 code

---
+++ index.js
@@ -0,0 +1,10 @@
+var express = require('express');
+
+// App setup
+var app = express();
+var server = app.listen(4000, function(){
+    console.log('listening for requests on port 4000,');
+});
+
+// Static files
+app.use(express.static('public'));
